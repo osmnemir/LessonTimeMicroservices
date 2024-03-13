078bef0e9693d97d1860997d0bf21149fbfad0a6
Osman Emir <74381219+osmnemir@users.noreply.github.com>
2024-03-14 01:15:58 +0300

    Ownedtypes Shadowproperty Backingfield

--- Services/Order/LessonTime.Services.Order.Domain/OrderAggregate/Order.cs
+++ Services/Order/LessonTime.Services.Order.Domain/OrderAggregate/Order.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LessonTime.Services.Order.Domain.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,42 @@ using System.Threading.Tasks;
 
 namespace LessonTime.Services.Order.Domain.OrderAggregate
 {
-    internal class Order
+    public class Order : Entity, IAggrageteRoot
     {
+        public DateTime CreatedDate { get; private set; }
+
+        public Address Address { get; private set; }
+
+        public string BuyerId { get; private set; }
+
+        private readonly List<OrderItem> _orderItems;
+
+        public IReadOnlyCollection<OrderItem> OrderItems => _orderItems;
+
+        public Order()
+        {
+        }
+
+        public Order(string buyerId, Address address)
+        {
+            _orderItems = new List<OrderItem>();
+            CreatedDate = DateTime.Now;
+            BuyerId = buyerId;
+            Address = address;
+        }
+
+        public void AddOrderItem(string productId, string productName, decimal price, string pictureUrl)
+        {
+            var existProduct = _orderItems.Any(x => x.ProductId == productId);
+
+            if (!existProduct)
+            {
+                var newOrderItem = new OrderItem(productId, productName, pictureUrl, price);
+
+                _orderItems.Add(newOrderItem);
+            }
+        }
+
+        public decimal GetTotalPrice => _orderItems.Sum(x => x.Price);
     }
 }
--- Services/Order/LessonTime.Services.Order.Domain/OrderAggregate/Orderİtem.cs
+++ Services/Order/LessonTime.Services.Order.Domain/OrderAggregate/Orderİtem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LessonTime.Services.Order.Domain.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,30 @@ using System.Threading.Tasks;
 
 namespace LessonTime.Services.Order.Domain.OrderAggregate
 {
-    internal class Orderİtem
+    public class OrderItem : Entity
     {
+        public string ProductId { get; private set; }
+        public string ProductName { get; private set; }
+        public string PictureUrl { get; private set; }
+        public Decimal Price { get; private set; }
+
+        public OrderItem()
+        {
+        }
+
+        public OrderItem(string productId, string productName, string pictureUrl, decimal price)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            PictureUrl = pictureUrl;
+            Price = price;
+        }
+
+        public void UpdateOrderItem(string productName, string pictureUrl, decimal price)
+        {
+            ProductName = productName;
+            Price = price;
+            PictureUrl = pictureUrl;
+        }
     }
 }
