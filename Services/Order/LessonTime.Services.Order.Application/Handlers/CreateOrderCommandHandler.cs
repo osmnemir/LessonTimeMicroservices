13c7217555142484df965ff16d11ffc3f10baed9
Osman Emir <74381219+osmnemir@users.noreply.github.com>
2024-03-15 00:22:38 +0300

    QueryHandler

---
+++ Services/Order/LessonTime.Services.Order.Application/Handlers/CreateOrderCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using LessonTime.Services.Order.Application.Dtos;
+using LessonTime.Services.Order.Domain.OrderAggregate;
+using LessonTime.Services.Order.Infrastructure;
+using LessonTime.Shared.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LessonTime.Services.Order.Application.Handlers
+{
+    public class CreateOrderCommandHandler
+    {
+
+    }
+       
+    }
+
