982da1f5833fa4db9a8937de2182cf4e4b52b4f5
Osman Emir <74381219+osmnemir@users.noreply.github.com>
2024-03-15 00:14:42 +0300

    Queries

---
+++ Services/Order/LessonTime.Services.Order.Application/Queries/GetOrdersByUserIdQuer.cs
@@ -0,0 +1,17 @@
+﻿using LessonTime.Services.Order.Application.Dtos;
+using LessonTime.Shared.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonTime.Services.Order.Application.Queries
+{
+    public class GetOrdersByUserIdQuery : IRequest<Response<List<OrderDto>>>
+    {
+        public string UserId { get; set; }
+    }
+}
+ 
