8490c9327d80dc9bc1353064cf5f66e0342e6fe5
Osman Emir <74381219+osmnemir@users.noreply.github.com>
2024-03-19 01:57:01 +0300

    ServiceApiSettings

---
+++ Frontends/LessonTime.WEB/Models/ServiceApiSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace LessonTime.WEB.Models
+{
+    public class ServiceApiSettings
+    {
+        public string BaseUri { get; set; }
+        public string PhotoStockUri { get; set; }
+    }
+}
---
+++ Frontends/LessonTime.WEB/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,15 @@
+﻿using IdentityModel.Client;
+using LessonTime.Shared.Dtos;
+using LessonTime.WEB.Models;
+using System.Threading.Tasks;
+
+namespace LessonTime.WEB.Services.Interfaces
+{
+    public interface IIdentityService
+    {
+        Task<Response<bool>> SingIn(SignInInput signInInput);
+        Task<TokenResponse> GetAccessTokenByRefreshToken();
+        Task RevokeRefreshToken();
+
+    }
+}
