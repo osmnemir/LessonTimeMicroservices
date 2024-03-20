f79cddee750873fb3b17a17ae7668a70f0183077
Osman Emir <74381219+osmnemir@users.noreply.github.com>
2024-03-21 01:53:42 +0300

    AuthController

---
+++ Frontends/LessonTime.WEB/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using LessonTime.WEB.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System;
+using LessonTime.WEB.Services.Interfaces;
+
+namespace LessonTime.WEB.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IIdentityService _identityService;
+
+        public AuthController(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignIn(SignInInput signinInput)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var response = await _identityService.SignIn(signinInput);
+
+            if (!response.IsSuccessful)
+            {
+                response.Errors.ForEach(x =>
+                {
+                    ModelState.AddModelError(String.Empty, x);
+                });
+
+                return View();
+            }
+
+            return RedirectToAction(nameof(Index), "Home");
+        }
+    }
+}
