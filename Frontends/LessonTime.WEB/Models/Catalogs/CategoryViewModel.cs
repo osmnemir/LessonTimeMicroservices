ca03c115ca0de523aba322c22190be4ae760a5da
Osman Emir <74381219+osmnemir@users.noreply.github.com>
2024-03-23 02:20:32 +0300

    ICatalogService / Models.Catalogs add

---
+++ Frontends/LessonTime.WEB/Models/Catalogs/CategoryViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LessonTime.WEB.Models.Catalogs
+{
+    public class CategoryViewModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ Frontends/LessonTime.WEB/Services/Interfaces/ICatalogService.cs
@@ -0,0 +1,23 @@
+﻿using LessonTime.WEB.Models.Catalogs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LessonTime.WEB.Services.Interfaces
+{
+    public interface ICatalogService
+    {
+        Task<List<CourseViewModel>> GetAllCourseAsync();
+
+        Task<List<CategoryViewModel>> GetAllCategoryAsync();
+
+        Task<List<CourseViewModel>> GetAllCourseByUserIdAsync(string userId);
+
+        Task<CourseViewModel> GetByCourseId(string courseId);
+
+        Task<bool> CreateCourseAsync(CourseCreateInput courseCreateInput);
+
+        Task<bool> UpdateCourseAsync(CourseUpdateInput courseUpdateInput);
+
+        Task<bool> DeleteCourseAsync(string courseId);
+    }
+}
