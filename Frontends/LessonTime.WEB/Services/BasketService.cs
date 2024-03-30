ea5869fe999b085a21ffc9702de46d3fc6ff8e93
Osman Emir <74381219+osmnemir@users.noreply.github.com>
2024-03-31 02:16:32 +0300

    ServiceExtensions and IBasketService

---
+++ Frontends/LessonTime.WEB/Services/BasketService.cs
@@ -0,0 +1,17 @@
+﻿using LessonTime.Services.Discount.Services;
+using LessonTime.Shared.Dtos;
+using LessonTime.WEB.Models.Baskets;
+using LessonTime.WEB.Services.Interfaces;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace LessonTime.WEB.Services
+{
+    public class BasketService:IBasketService
+
+    {
+        
+    }
+}
--- Frontends/LessonTime.WEB/Startup.cs
+++ Frontends/LessonTime.WEB/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using LessonTime.WEB.Handler;
 using LessonTime.Shared.Services;
 using LessonTime.WEB.Helpers;
+using LessonTime.WEB.Extensions;
 
 namespace LessonTime.WEB
 {
@@ -33,45 +34,18 @@ namespace LessonTime.WEB
             services.AddSingleton<PhotoHelper>();
             services.AddScoped<ISharedIdentityService, SharedIdentityService>();
 
-            var serviceApiSettings = Configuration.GetSection("ServiceApiSettings").Get<ServiceApiSettings>();
-
-            services.AddHttpClient<IClientCredentialTokenService, ClientCredentialTokenService>();
-
             services.AddScoped<ResourceOwnerPasswordTokenHandler>();
             services.AddScoped<ClientCredentialTokenHandler>();
-            services.AddScoped<IIdentityService, IdentityService>();
 
-            services.AddHttpClient<ICatalogService, CatalogService>(opt =>
-            {
-                opt.BaseAddress = new Uri($"{serviceApiSettings.GatewayBaseUri}/{serviceApiSettings.Catalog.Path}");
-            }).AddHttpMessageHandler<ClientCredentialTokenHandler>();
-
-            services.AddHttpClient<IPhotoStockService, PhotoStockService>(opt =>
-            {
-                opt.BaseAddress = new Uri($"{serviceApiSettings.GatewayBaseUri}/{serviceApiSettings.PhotoStock.Path}");
-            }).AddHttpMessageHandler<ClientCredentialTokenHandler>();
-
-            services.AddHttpClient<IUserService, UserService>(opt =>
-            {
-                opt.BaseAddress = new Uri(serviceApiSettings.IdentityBaseUri);
-            }).AddHttpMessageHandler<ResourceOwnerPasswordTokenHandler>();
+            services.AddHttpClientServices(Configuration);
 
 
-
-
-
-
-            services.AddHttpClient<IdentityService>();
-            services.AddHttpClient<CatalogService>();
-
-
-
-           
-
+            //services.AddScoped<IIdentityService, IdentityService>();      
+            //services.AddHttpClient<IdentityService>();
+            //services.AddHttpClient<CatalogService>();
 
 
             services.AddControllersWithViews();
-
 
 
             services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, opts =>
