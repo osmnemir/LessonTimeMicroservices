49489d9ca2599f985e79f52f28e37abf5d0fa714
Osman Emir <74381219+osmnemir@users.noreply.github.com>
2024-04-02 23:30:55 +0300

    DiscountViewModel and DiscountValidator

---
+++ Frontends/LessonTime.WEB/Validators/DiscountApplyInputValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using LessonTime.WEB.Models.Discount;
+
+namespace LessonTime.WEB.Validators
+{
+    public class DiscountApplyInputValidator : AbstractValidator<DiscountApplyInput>
+    {
+        public DiscountApplyInputValidator()
+        {
+            RuleFor(x => x.Code).NotEmpty().WithMessage("indirim kupon alanı boş olamaz");
+
+        }
+    }
+}
